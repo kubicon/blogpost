24f7e02a5af18e806c6d88188009a7ad23f1e417
kubicon3 <kubicon3@fel.cvut.cz>
2026-08-22 01:08:18 +0200

    Changed the layout. Removed the default one and added linkedin and github profiles to the top left corner

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,13 @@
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export interface Project {
+	title: string;
+	href: string;
+}
+
+export const PROJECTS: Project[] = [
+	{ title: 'Project One', href: '#' },
+	{ title: 'Project Two', href: '#' },
+];
